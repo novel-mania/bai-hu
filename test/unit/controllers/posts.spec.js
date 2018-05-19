563f891396c03e682fdef0e83e98bc932f9c7664
Henrique C Tatagiba <henriquectatagiba@gmail.com>
2018-05-19 17:24:11 +0000

    add posts unit tests

--- test/unit/controllers/posts.spec.js
+++ test/unit/controllers/posts.spec.js
@@ -87,5 +87,89 @@ describe('Controllers: Posts', () => {
       return postsController.create(defaultPost)
         .then(post => expect(post.data).to.be.eql(defaultPost));
     });
+    context('when an error ocurs', () => {
+      it('should return 422', () => {
+        stub.withArgs().rejects({ message: 'Error' });
+
+        const postsController = new PostsController(Posts);
+        return postsController.create(defaultPost)
+          .catch(err => expect(err).to.be.eql({ message: 'Error' }));
+      });
+    });
+  });
+  describe('put()', () => {
+    before(() => {
+      stub = sinon.stub(Posts, 'findOneAndUpdate');
+    });
+    after(() => {
+      stub.restore();
+    });
+
+    it('should update an existent post', () => {
+      const id = '1';
+      const updatedPost = {
+        id,
+        title: 'Novo website da novelmania!',
+        content: 'Esse é o novo site, post atualizado.',
+        author: '56cb91bdc3464f14678934ca',
+        book: '56cb91bdc3464f14678934ca',
+        tags: 'Novidades',
+      };
+
+      stub.withArgs({ _id: id }, updatedPost).resolves(updatedPost);
+
+      const postsController = new PostsController(Posts);
+      return postsController.update(id, updatedPost)
+        .then(post => expect(post.data).to.be.eql(updatedPost));
+    });
+
+    context('when an error ocurs', () => {
+      it('should return 422', () => {
+        const id = '1';
+        const updatedPost = {
+          id,
+          title: 'Novo website da novelmania!',
+          content: 'Esse é o novo site, post atualizado.',
+          author: '56cb91bdc3464f14678934ca',
+          book: '56cb91bdc3464f14678934ca',
+          tags: 'Novidades',
+        };
+
+        stub.withArgs({ _id: id }, updatedPost).rejects({ message: 'Error' });
+
+        const postsController = new PostsController(Posts);
+        return postsController.update(id, updatedPost)
+          .catch(err => expect(err).to.be.eql({ message: 'Error' }));
+      });
+    });
+  });
+  describe('delete()', () => {
+    before(() => {
+      stub = sinon.stub(Posts, 'remove');
+    });
+
+    after(() => {
+      stub.restore();
+    });
+
+    it('should delete a existent post', () => {
+      const id = '1';
+      stub.withArgs({ _id: id }).resolves([1]);
+
+      const postsController = new PostsController(Posts);
+      return postsController.remove(id)
+        .then(post => expect(post).to.be.eql([1]));
+    });
+
+    context('when an error ocurs', () => {
+      it('should return 400', () => {
+        const id = '1';
+        stub.withArgs({ _id: id }).rejects({ message: 'Error' });
+
+        const postsController = new PostsController(Posts);
+        return postsController.remove(id)
+          .catch(err => expect(err).to.be.eql({ message: 'Error' }));
+      });
+    });
   });
 });
